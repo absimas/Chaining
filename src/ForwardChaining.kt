dffd770059ae31d7819a81c85d425c70a9d12f25
Simas <ab.simas@gmail.com>
2017-10-29 09:41:04 +0200

    Parse input

--- src/ForwardChaining.kt
+++ src/ForwardChaining.kt
@@ -1,6 +1,11 @@
+import java.io.File
+
 /**
  * Created by Simas on 2017 Oct 29.
  */
+private const val RULE_PREFIX = "1) Rules"
+private const val FACT_PREFIX = "2) Facts"
+private const val TARGET_PREFIX = "3) Target"
 class ForwardChaining {
 
   companion object {
@@ -8,6 +13,81 @@ class ForwardChaining {
     fun main(args: Array<String>) {
       ForwardChaining()
     }
+  }
+
+  class Rule(val name: String, val sources: MutableList<String>, val destination: String) {
+    override fun toString(): String {
+      return name
+    }
+  }
+
+  private lateinit var target: String
+
+  private val rules = mutableListOf<Rule>()
+  private val facts = mutableListOf<String>()
+
+  init {
+    parseInput()
+
+    println("Rules: $rules")
+    println("Facts: $facts")
+    println("Target: $target")
+  }
+
+  private fun parseInput() {
+    val input = File("input")
+    if (!input.exists()) {
+      throw IllegalStateException("Input file must exist!")
+    }
+
+    val lines = input.readLines()
+    val lineCount = lines.size
+    if (lineCount <= 1) {
+      throw IllegalStateException("Input file must be separated by new lines is empty!")
+    }
+
+    // Fetch indexes
+    val ruleIndex = lines.indexOfFirst { it.startsWith(RULE_PREFIX) }
+    if (ruleIndex == -1) throw IllegalStateException("Rules were not found in the input file!")
+    val factIndex = lines.indexOfFirst { it.startsWith(FACT_PREFIX) }
+    if (factIndex == -1) throw IllegalStateException("Facts were not found in the input file!")
+    val targetIndex = lines.indexOfFirst { it.startsWith(TARGET_PREFIX) }
+    if (targetIndex == -1) throw IllegalStateException("Target wasn't not found in the input file!")
+
+    // Rules
+    // Extract rules
+    val ruleLines = lines.subList(ruleIndex+1, factIndex)
+
+    // Remove comments and trim
+    val cleanRules = ruleLines.map {
+      it.replace(Regex("//.*"), "").trim()
+    }.toMutableList()
+
+    // Remove empty lines
+    cleanRules.removeIf {
+      it.isEmpty()
+    }
+
+    // Extract arguments into a Rule class
+    cleanRules.forEach { rule ->
+      val split = rule.split(' ')
+      if (split.size <= 1) {
+        throw IllegalStateException("Incorrect rule arguments found! Line was $rule.")
+      }
+
+      val name = "R${rules.size+1}"
+      val sources = split.subList(1, split.size)
+      val destination = split[0]
+
+      rules.add(Rule(name, sources.toMutableList(), destination))
+    }
+
+    // Facts
+    val factLine = lines[factIndex+1]
+    facts.addAll(factLine.split(' '))
+
+    // Target
+    target = lines[targetIndex+1]
   }
 
 }
