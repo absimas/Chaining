976c6426d870adce96bf4e373d17c9dddb3663c1
Simas <ab.simas@gmail.com>
2017-10-29 09:40:49 +0200

    Init

---
+++ src/ForwardChaining.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by Simas on 2017 Oct 29.
+ */
+class ForwardChaining {
+
+  companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+      ForwardChaining()
+    }
+  }
+
+}
